a4eb90344832f5fb17eb12a58abdcb9e11289418
LizzyKate <onyenekwelizabeth@gmail.com>
2022-03-19 12:19:12 +0100

    remove flashing of content

--- js/animation.js
+++ js/animation.js
@@ -5,10 +5,10 @@ let editor = document.querySelector(".editor_code")
 
 const hero = gsap.timeline({ defaults: { opacity: 0, stagger: 0.2, display: "none", autoAlpha: 0 }, repeat: -1 })
 function init() {
-    hero.from(".anchor_hero", { autoAlpha: 0, repeat: 0 })
+    hero.from(".anchor_hero", { autoAlpha: 0, duration: 0.1 })
         .from(".hero_img", { ease: "linear", rotate: 270, transformOrigin: "50% 50%", onComplete: disappear, duration: 1 })
         .from(".editor_code", { onComplete: code, duration: 5 })
-        .from(".phone_img", {})
+        .from(".phone_img", { autoAlpha: 1 })
 
 
     function disappear() {
