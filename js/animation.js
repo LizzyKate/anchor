e82fb8d2fbec35ad9c70aca83d50832710cd1f80
LizzyKate <onyenekwelizabeth@gmail.com>
2022-03-19 11:47:59 +0100

    remove flashing of content

--- js/animation.js
+++ js/animation.js
@@ -4,19 +4,26 @@ let card = document.querySelector(".hero_img")
 let editor = document.querySelector(".editor_code")
 
 const hero = gsap.timeline({ defaults: { opacity: 0, stagger: 0.2, display: "none", autoAlpha: 0 }, repeat: -1 })
-hero.from(".hero_img", { ease: "linear", rotate: 270, transformOrigin: "50% 50%", onComplete: disappear, duration: 1 })
-    .from(".editor_code", { autoAlpha: 1, onComplete: code, duration: 5 })
-    .from(".phone_img", { autoAlpha: 1 })
+function init() {
+    hero.from(".hero_img", { ease: "linear", rotate: 270, transformOrigin: "50% 50%", onComplete: disappear, duration: 1 })
+        .from(".editor_code", { onComplete: code, duration: 5 })
+        .from(".phone_img", {})
 
 
-function disappear() {
-    card.style.display = 'none'
+    function disappear() {
+        card.style.display = 'none'
+    }
+    function code() {
+        editor.style.display = 'none'
+    }
 }
-function code() {
-    editor.style.display = 'none'
-}
+
 const typewriter = gsap.timeline({ defaults: { repeat: -1, repeatDelay: 6 } })
 typewriter.to(".first", { duration: 1, text: ". container {" })
     .to(".second", { duration: 1, text: " width: 590px;" })
     .to(".third", { duration: 1, text: " height: 374px;" })
     .to(".fourth", { duration: 1, text: " }" })
+
+window.addEventListener("load", function (event) {
+    init()
+})
