8dddd75c90a71dad99cb4da3e876c95b16ca8088
LizzyKate <onyenekwelizabeth@gmail.com>
2022-03-20 14:42:29 +0100

    80% done

--- js/animation.js
+++ js/animation.js
@@ -6,7 +6,7 @@ let editor = document.querySelector(".editor_code")
 const hero = gsap.timeline({ defaults: { opacity: 0, stagger: 0.2, display: "none", autoAlpha: 0 }, repeat: -1 })
 function init() {
     hero.from(".anchor_hero", { autoAlpha: 0, duration: 0.001 })
-        .from(".hero_img", { ease: "linear", rotate: 270, transformOrigin: "50% 50%", onComplete: disappear, duration: 2 })
+        .from(".hero_img", { ease: "linear", rotate: 90, transformOrigin: "50% 50%", onComplete: disappear, duration: 2 })
         .from(".editor_code", { onComplete: code, duration: 5 })
         .from(".phone_img", { autoAlpha: 1, duration: 2 })
 
@@ -19,7 +19,7 @@ function init() {
     }
 }
 
-const typewriter = gsap.timeline({ defaults: { repeat: -1, repeatDelay: 6 } })
+const typewriter = gsap.timeline({ repeat: -1, repeatDelay: 1, delay: 2 })
 typewriter.to(".first", { duration: 1, text: ". container {" })
     .to(".second", { duration: 1, text: " width: 590px;" })
     .to(".third", { duration: 1, text: " height: 374px;" })
@@ -29,7 +29,9 @@ window.addEventListener("load", function (event) {
     init()
 })
 
-const typing = gsap.timeline({ defaults: { repeat: -1, repeatDelay: 2 } })
+
+const typing = gsap.timeline({ repeat: -1, repeatDelay: 2 })
+
 typing.to(".fifth", { duration: 1, text: ". container {" })
     .to(".sixth", { duration: 1, text: " width: 590px;" })
     .to(".seventh", { duration: 1, text: " height: 374px;" })
